889d08b2179cc4cfc1c423006c7aac4514c37f2e
abhi <abhi@abhi-PC>
2022-02-22 23:18:24 +0530

    dynamic

---
+++ pack1/src/pack11/B.java
@@ -0,0 +1,13 @@
+package pack11;
+
+import pack11.pack2.A;
+
+public class B {
+
+	public static void main(String[] args) {
+	A a=new A();
+	a.m1();
+
+	}
+
+}
---
+++ pack1/src/pack11/pack2/A.java
@@ -0,0 +1,11 @@
+package pack11.pack2;
+
+public class A {
+	public void m1() {
+		
+		System.out.println("m1 method");
+		
+		
+	}
+
+}
